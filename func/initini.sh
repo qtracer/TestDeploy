c06929a24f3121c1fd325fd30a569ffbbfb103f9
gaohuajun <3011483811@qq.com>
2021-06-25 15:18:47 +0800

    修改initini

---
+++ func/initini.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+workdir=$1
+
+# 初始化配置
+sed -i '1,$d' ${workdir}/ini/container.ini
+sed -i '1,$d' ${workdir}/ini/global.ini
+sed -i 's/true/false/g' ${workdir}/ini/store.ini
+
+# 删除日志
+cd ${workdir}/log && rm -rf *
