5c0ee1b8f6ef1c03224bc37ee612b163d0b223b5
汐度清风 <59275975+qtracer@users.noreply.github.com>
2023-05-26 15:16:55 +0800

    Update installDockerCompose.sh

--- func/installDockerCompose.sh
+++ func/installDockerCompose.sh
@@ -14,7 +14,7 @@ else
   # 国外镜像
   # curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   # 国内镜像
-  curl -L "https://get.daocloud.io/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  curl -L "https://get.daocloud.io/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 fi
 
