6c8ae8976c8c94cfeaf8295ab86866d870b65736
汐度清风 <59275975+qtracer@users.noreply.github.com>
2023-06-03 16:25:25 +0800

    Update timeSync.sh

--- func/timeSync.sh
+++ func/timeSync.sh
@@ -6,6 +6,10 @@ workdir=$1
 export info="$0: timeSync for the machine"
 bash ${workdir}/comm/echoInfo.sh $workdir
 
+# 修改时区
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+# 时间同步
 yum install chrony -y
 sed 's/0.centos.pool.ntp.org/time2.aliyun.com/' /etc/chrony.conf
 systemctl start chronyd
