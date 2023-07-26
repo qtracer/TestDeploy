f2636816f2ea9eee6828b19213b3da9fa55d40af
汐度清风 <59275975+qtracer@users.noreply.github.com>
2023-07-26 09:35:27 +0800

    Update timeSync.sh

--- func/timeSync.sh
+++ func/timeSync.sh
@@ -4,6 +4,8 @@ workdir=$1
 export info="$0: timeSync for the machine"
 bash $workdir/comm/echoInfo.sh $workdir
 
+ifexist=$(cat ${workdir}/ini/config.ini | grep "installedEnv" | awk -F = '{print $2}')
+
 [ "`rpm -qa | egrep ^rdate`" ] || yum -y install rdate > /dev/null 2>&1
 tz=`date -R | awk '{print $NF}'`
 if [ $tz != "+0800" ]
@@ -12,11 +14,13 @@ if [ $tz != "+0800" ]
         echo ZONE=\"Asia/Shanghai\" > /etc/sysconfig/clock
 fi
 
-yum install ntpdate -y
-/usr/sbin/ntpdate us.ntp.org.cn
-if [ $? != 0 ]
-   then
-      /usr/bin/rdate -s time.nist.gov
-fi
+if [ "$ifexist" = "false" ];then
+  yum install ntpdate -y
+  /usr/sbin/ntpdate us.ntp.org.cn
+  if [ $? != 0 ]
+     then
+        /usr/bin/rdate -s time.nist.gov
+  fi
 
-/sbin/hwclock -w
+  /sbin/hwclock -w
+fi
