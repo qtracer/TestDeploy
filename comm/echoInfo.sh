1842c07a65b908119b204fec5d7dfcfdd430ebaf
汐度清风 <59275975+qtracer@users.noreply.github.com>
2024-06-14 17:54:49 +0800

    Update echoInfo.sh

--- comm/echoInfo.sh
+++ comm/echoInfo.sh
@@ -4,3 +4,4 @@ workdir=$1
 curdate=$(cat ${workdir}/ini/global.ini | grep "curdate" | awk -F = '{print $2}')
 
 echo "++++++++++$(date +%Y-%m-%d' '%H:%M:%S.%N | cut -b 1-23) ${info}++++++++++" | tee -a ${workdir}/log/${curdate}.log
+
