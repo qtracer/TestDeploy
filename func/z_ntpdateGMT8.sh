e195de6060d66b1533ad84daaace94925d45b144
汐度清风 <59275975+qtracer@users.noreply.github.com>
2024-08-30 11:32:41 +0800

    Update z_ntpdateGMT8.sh

--- func/z_ntpdateGMT8.sh
+++ func/z_ntpdateGMT8.sh
@@ -4,21 +4,8 @@ workdir=$1
 export info="$0: timeSync for the machine"
 bash $workdir/comm/echoInfo.sh $workdir
 
-ifexist=$(cat ${workdir}/ini/config.ini | grep "installedEnv" | awk -F = '{print $2}')
-
-#[ "`rpm -qa | egrep ^rdate`" ] || yum -y install rdate > /dev/null 2>&1
-#tz=`date -R | awk '{print $NF}'`
-#if [ $tz != "+0800" ]
-#   then
-#       \cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-#        echo ZONE=\"Asia/Shanghai\" > /etc/sysconfig/clock
-#fi
-
-yum install ntpdate -y
-/usr/sbin/ntpdate us.ntp.org.cn
-if [ $? != 0 ]
-   then
-      /usr/bin/rdate -s time.nist.gov
+rpm -qi ntpdate > /dev/null
+if [ $? -ne 0 ];then
+  yum install ntpdate -y
+  /usr/sbin/ntpdate ntp.aliyun.com
 fi
-
-/sbin/hwclock -w
