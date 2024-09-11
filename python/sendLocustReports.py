3c6527d8ddf0361ca046a56922c36da78a94b8a3
汐度清风 <59275975+qtracer@users.noreply.github.com>
2024-09-11 17:44:42 +0800

    Update sendLocustReports.py

--- python/sendLocustReports.py
+++ python/sendLocustReports.py
@@ -64,7 +64,7 @@ def send_email(subject, dicbody, file=None):
 
     starttime=dicbody['starttime']
     endtime=dicbody['endtime']
-    bodytext=f"starttime:GMT{starttime}\nendtime:GMT{endtime}\n详细见附件"
+    bodytext=f"starttime:{starttime}GMT\nendtime:{endtime}GMT\n详细见附件"
     mail.attach(MIMEText(bodytext, 'plain', 'utf-8'))
 
     # 附件
